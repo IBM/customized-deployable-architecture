cc940fb088672b95948b9fbbaf4d2c8d264f647e
Keith Biegert <kbiegert@us.ibm.com>
2022-11-03 17:17:56 -0400

    refactor cleanup script

--- .github/scripts/cleanup.sh
+++ .github/scripts/cleanup.sh
@@ -8,47 +8,32 @@ function getWorkspaceStatus() {
     ibmcloud schematics workspace get --id "$WORKSPACE_ID" --output json | jq -r '.status'
 }
 
-CATALOG_NAME=$1
-OFFERING_NAME=$2
-VERSION=$3
+function deleteWorkspace() {
+    local destroyStatus=$(getWorkspaceStatus)
 
-echo "cleaning up workspaces and resources"
+    if [ "$destroyStatus" != "FAILED" ] && [ -n "$destroyStatus" ]
+    then
+        echo "delete workspace"
+        local attempts=0
+        local ret=1
+        while [[ ret -ne 0 ]] && [[ $attempts -le 3 ]]
+        do
+            ret=0
+            ibmcloud schematics workspace delete --id "$WORKSPACE_ID" -f || ret=$?
+            if [[ ret -ne 0 ]]
+            then
+                attempts=$((attempts+1))
+                echo "failure number ${attempts} to delete workspace"
+                sleep 15
+            fi
+        done
+    fi
+}
 
-# refresh token
-ibmcloud catalog utility netrc
-
-# get the schematics workspace id that was used for the validation of this version
-WORKSPACE_ID=$(getWorkspaceId)
-
-echo "workspace id: $WORKSPACE_ID"
-workspaceStatus=$(getWorkspaceStatus)
-echo "workspace status: ${workspaceStatus}"
-
-if [ "$workspaceStatus" != "INACTIVE" ]
-then
-    echo "destroying workspace resources"
-    ibmcloud schematics destroy --id "$WORKSPACE_ID" -f
-
-    echo "waiting for resources to be destroyed"
-    # wait for destroy workspace to be started
-    attempts=0
-    destroyStarted=$(getWorkspaceStatus)
-    while [ "$destroyStarted" != "INPROGRESS" ] && [[ $attempts -le 60 ]]
-    do
-        sleep 5
-        destroyStarted=$(getWorkspaceStatus)
-        attempts=$((attempts+1))
-        if [[ counter -ge 30 ]]
-        then
-            echo "workspace status: ${destroyStatus} . failed to start destroy after 5 minutes.  giving up."
-            exit 1
-        fi
-    done
-    echo "workspace destroy resources started"
-
-    attempts=0
-    counter=0
-    destroyStatus=incomplete
+function queryAndWaitForWorkspace() {
+    local attempts=0
+    local counter=0
+    local destroyStatus=incomplete
     while [ "$destroyStatus" != "INACTIVE" ] && [ "$destroyStatus" != "FAILED" ] && [[ $attempts -le 3 ]]
     do
         sleep 10
@@ -71,55 +56,65 @@ then
             echo "workspace resource destroy status: ${destroyStatus}"
         fi
     done
-    
-    # max attempts reached or the resource destroy failed.  try to destroy again.
-    if [ "$destroyStatus" == "FAILED" ]
+}
+
+function destroyWorkspaceResources() {
+    # refresh token
+    ibmcloud catalog utility netrc
+
+    # get the schematics workspace id that was used for the validation of this version
+    WORKSPACE_ID=$(getWorkspaceId)
+
+    echo "workspace id: $WORKSPACE_ID"
+    workspaceStatus=$(getWorkspaceStatus)
+    echo "workspace status: ${workspaceStatus}"
+
+    if [ "$workspaceStatus" != "INACTIVE" ]
     then
-        echo "workspace resource destroy failed - retrying"
+        echo "destroying workspace resources"
+        ibmcloud schematics destroy --id "$WORKSPACE_ID" -f
+
+        echo "waiting for resources to be destroyed"
+        # wait for destroy workspace to be started
         attempts=0
-        counter=0
-        destroyStatus=incomplete
-        while [ "$destroyStatus" != "INACTIVE" ] && [ "$destroyStatus" != "FAILED" ] && [[ $attempts -le 3 ]]
+        destroyStarted=$(getWorkspaceStatus)
+        while [ "$destroyStarted" != "INPROGRESS" ] && [[ $attempts -le 60 ]]
         do
-            sleep 10
-            counter=$((counter+1))
+            sleep 5
+            destroyStarted=$(getWorkspaceStatus)
+            attempts=$((attempts+1))
             if [[ counter -ge 30 ]]
             then
-                echo "destroy status: ${destroyStatus}"
-                counter=0
-            fi
-            ret=0
-            prevDestroyStatus="$destroyStatus"
-            destroyStatus=$(getWorkspaceStatus) || ret=$?
-            if [[ ret -ne 0 ]]
-            then
-                attempts=$((attempts+1))
-                echo "failure number ${attempts} to check destroy status"
-            fi
-            if [ "$prevDestroyStatus" != "$destroyStatus" ]
-            then
-                echo "destroy status: ${destroyStatus}"
+                echo "workspace status: ${destroyStatus} . failed to start destroy after 5 minutes.  giving up."
+                exit 1
             fi
         done
-    fi
-fi
+        echo "workspace destroy resources started"
 
-echo "final destroy status: ${destroyStatus}"
-
-if [ "$destroyStatus" != "FAILED" ] && [ -n "$destroyStatus" ]
-then
-    echo "delete workspace"
-    attempts=0
-    ret=1
-    while [[ ret -ne 0 ]] && [[ $attempts -le 3 ]]
-    do
-        ret=0
-        ibmcloud schematics workspace delete --id "$WORKSPACE_ID" -f || ret=$?
-        if [[ ret -ne 0 ]]
+        queryAndWaitForWorkspace
+        destroyStatus=$(getWorkspaceStatus)
+        
+        # max attempts reached or the resource destroy failed.  try to destroy again.
+        if [ "$destroyStatus" == "FAILED" ]
         then
-            attempts=$((attempts+1))
-            echo "failure number ${attempts} to delete workspace"
-            sleep 15
+            echo "workspace resource destroy failed - retrying"
+            queryAndWaitForWorkspace
         fi
-    done
-fi
+    fi
+
+    destroyStatus=$(getWorkspaceStatus)
+    echo "final resource destroy status: ${destroyStatus}"
+}
+
+# ------------------------------------------------------------------------------------
+#  main
+# ------------------------------------------------------------------------------------
+
+CATALOG_NAME=$1
+OFFERING_NAME=$2
+VERSION=$3
+
+echo "cleaning up workspaces and resources"
+
+destroyWorkspaceResources
+deleteWorkspace
