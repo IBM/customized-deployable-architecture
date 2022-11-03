78b77894b646988ec9a99a5762371e6a15497518
Keith Biegert <kbiegert@us.ibm.com>
2022-11-03 16:10:37 -0400

    refactor cleanup script

--- .github/scripts/cleanup.sh
+++ .github/scripts/cleanup.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+function getWorkspaceId() {
+    ibmcloud catalog offering get -c "$CATALOG_NAME" -o "$OFFERING_NAME" --output json | jq -r --arg version "$VERSION" '.kinds[] | select(.format_kind=="terraform").versions[] | select(.version==$version).validation.target.workspace_id'
+}
+
+function getWorkspaceStatus() {
+    ibmcloud schematics workspace get --id "$WORKSPACE_ID" --output json | jq -r '.status'
+}
+
 CATALOG_NAME=$1
 OFFERING_NAME=$2
 VERSION=$3
@@ -10,10 +18,10 @@ echo "cleaning up workspaces and resources"
 ibmcloud catalog utility netrc
 
 # get the schematics workspace id that was used for the validation of this version
-WORKSPACE_ID=$(ibmcloud catalog offering get -c "$CATALOG_NAME" -o "$OFFERING_NAME" --output json | jq -r --arg version "$VERSION" '.kinds[] | select(.format_kind=="terraform").versions[] | select(.version==$version).validation.target.workspace_id')
+WORKSPACE_ID=$(getWorkspaceId)
 
 echo "workspace id: $WORKSPACE_ID"
-workspaceStatus=$(ibmcloud schematics workspace get --id "$WORKSPACE_ID" --output json | jq -r '.status')
+workspaceStatus=$(getWorkspaceStatus)
 echo "workspace status: ${workspaceStatus}"
 
 if [ "$workspaceStatus" != "INACTIVE" ]
@@ -22,39 +30,56 @@ then
     ibmcloud schematics destroy --id "$WORKSPACE_ID" -f
 
     echo "waiting for resources to be destroyed"
-    sleep 5
+    # wait for destroy workspace to be started
+    attempts=0
+    destroyStarted=$(getDestroyStatus)
+    while [ "$destroyStarted" != "INPROGRESS" ] && [[ $attempts -le 60 ]]
+    do
+        sleep 5
+        destroyStarted=$(getWorkspaceStatus)
+        attempts=$((attempts+1))
+        if [[ counter -ge 30 ]]
+        then
+            echo "workspace status: ${destroyStatus} . failed to start destroy after 5 minutes.  giving up."
+            exit 1
+        fi
+    done
+    echo "workspace destroy resources started"
+
     attempts=0
     counter=0
     destroyStatus=incomplete
-    while [ "$destroyStatus" != "COMPLETED" ] && [ "$destroyStatus" != "FAILED" ] && [[ $attempts -le 3 ]]
+    while [ "$destroyStatus" != "INACTIVE" ] && [ "$destroyStatus" != "FAILED" ] && [[ $attempts -le 3 ]]
     do
         sleep 10
         counter=$((counter+1))
         if [[ counter -ge 30 ]]
         then
-            echo "destroy status: ${destroyStatus}"
+            echo "workspace resource destroy status: ${destroyStatus}"
             counter=0
         fi
         ret=0
         prevDestroyStatus="$destroyStatus"
-        destroyStatus=$(ibmcloud schematics workspace action --id "$WORKSPACE_ID" --output json | jq -r '[.actions[] | select(.name=="DESTROY").status][0]') || ret=$?
+        destroyStatus=$(getWorkspaceStatus) || ret=$?
         if [[ ret -ne 0 ]]
         then
             attempts=$((attempts+1))
-            echo "failure number ${attempts} to check destroy status"
+            echo "failure number ${attempts} when attempting to check workspace resource destroy status"
         fi
         if [ "$prevDestroyStatus" != "$destroyStatus" ]
         then
-            echo "destroy status: ${destroyStatus}"
+            echo "workspace resource destroy status: ${destroyStatus}"
         fi
     done
+    
+    # max attempts reached or the resource destroy failed.  try to destroy again.
     if [ "$destroyStatus" == "FAILED" ]
     then
-        echo "destroy failed- retrying"
+        echo "workspace resource destroy failed - retrying"
         attempts=0
         counter=0
         destroyStatus=incomplete
-        while [ "$destroyStatus" != "COMPLETED" ] && [ "$destroyStatus" != "FAILED" ] && [[ $attempts -le 3 ]]
+        while [ "$destroyStatus" != "INACTIVE" ] && [ "$destroyStatus" != "FAILED" ] && [[ $attempts -le 3 ]]
         do
             sleep 10
             counter=$((counter+1))
@@ -65,7 +90,7 @@ then
             fi
             ret=0
             prevDestroyStatus="$destroyStatus"
-            destroyStatus=$(ibmcloud schematics workspace action --id "$WORKSPACE_ID" --output json | jq -r '[.actions[] | select(.name=="DESTROY").status][0]') || ret=$?
+            destroyStatus=$(getWorkspaceStatus) || ret=$?
             if [[ ret -ne 0 ]]
             then
                 attempts=$((attempts+1))
