acf44fe0095be1d0c1cef1045b9f9a9b96290f42
Gili Mendel <gmendel@us.ibm.com>
2022-12-06 12:29:14 -0500

    use git for now

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@
 ##############################################################################
 
 module "landing_zone" {
-  source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&catalogID=7df1e4ca-d54c-4fd0-82ce-3d13247308cd&flavor=standard&kind=terraform&name=slz-vpc-with-vsis&version=1.10.3"
+  # source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&catalogID=7df1e4ca-d54c-4fd0-82ce-3d13247308cd&flavor=standard&kind=terraform&name=slz-vpc-with-vsis&version=1.10.3"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi"
   prefix           = var.prefix
   region           = "eu-de"
   ibmcloud_api_key = var.ibmcloud_api_key
