20d299faedfecb405a488b1668cd5bf0a80b8f42
Keith Biegert <kbiegert@us.ibm.com>
2022-12-07 08:46:00 -0500

    variable text descriptions

--- examples/app/extension/variables.tf
+++ examples/app/extension/variables.tf
@@ -1,21 +1,27 @@
+# this is the output from an SLZ deployment.
 variable "config" {
     type = string
 }
 
+# install script for the application to be installed on the VSI
 variable "appInstall" {
     type = string
 }
 
+# the OS image to install on the VSI
+variable "image" {
+  default = "ibm-ubuntu-20-04-4-minimal-amd64-2"
+}
+
+# API key with sufficient permissions to deploy resources 
 variable "ibmcloud_api_key" {
   description = "The IBM Cloud platform API key needed to deploy IAM enabled resources."
   type        = string
   sensitive   = true
 }
 
+# configure the terraform provider and direct it to the same region where the SLZ resources 
+# were deployed
 variable "region" {
     type = string
-}
-
-variable "image" {
-  default = "ibm-ubuntu-20-04-4-minimal-amd64-2"
 }
