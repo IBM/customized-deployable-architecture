c7819e2271800f23858e6744d67a3f00e01dc597
Gili Mendel <gmendel@us.ibm.com>
2022-12-08 09:35:19 -0500

    dummy output

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "customSecInfra" {
   value       = module.landing_zone
   description = "Custom secure infrastructure configuration"
 }
+
+output "dummy" {
+  value       = "dumdum"
+  description = "just something"
+}
