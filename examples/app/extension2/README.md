886fdcc3fcfddaf311350a3a0f0d1d7d6a687ca1
Keith Biegert <kbiegert@us.ibm.com>
2023-01-10 14:45:14 -0500

    initial checkin of ansible framework

---
+++ examples/app/extension2/README.md
@@ -0,0 +1,6 @@
+# Application Extension for Custom Secure Infrastructure on VPC
+
+This example shows how an extension could be added on to an existing Custom Secure Infrastructure landscape by deploying an additional 
+Virtual Server into the Workload VPC.  The virtual server is initialized and configured as an *Apache web server*.
+
+The deployment mechanism for Apache onto the VSI is Ansible.
---
+++ examples/app/extension2/main.tf
@@ -0,0 +1,35 @@
+locals {
+
+  vpc_type = "workload"
+
+  subnet = join("-", [var.prefix, local.vpc_type, "vsi-zone-1"])
+}
+
+data "ibm_is_subnet" "subnet" {
+  name = local.subnet
+}
+
+data "ibm_is_ssh_key" "ssh-key" {
+  name = "${var.prefix}-ssh-key"
+}
+
+data "ibm_is_image" "image" {
+  name = var.image
+}
+
+module "slz_vsi" {
+  source                     = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vsi.git?ref=v1.1.5"
+  resource_group_id          = data.ibm_is_subnet.subnet.resource_group
+  image_id                   = data.ibm_is_image.image.id
+  create_security_group      = true
+  security_group             = var.appSecurityRules
+  tags                       = []
+  subnets                    = [{"name": local.subnet, "id": data.ibm_is_subnet.subnet.id, "zone":data.ibm_is_subnet.subnet.zone, "cidr": data.ibm_is_subnet.subnet.ipv4_cidr_block}]
+  vpc_id                     = data.ibm_is_subnet.subnet.vpc
+  prefix                     = "${var.prefix}-apache-webserver"
+  machine_type               = "cx2-2x4"
+  user_data                  = var.workLoadInitScript
+  boot_volume_encryption_key = null
+  vsi_per_subnet             = 1
+  ssh_key_ids                = [data.ibm_is_ssh_key.ssh-key.id]
+}
