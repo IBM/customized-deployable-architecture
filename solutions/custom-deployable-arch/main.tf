fa64114481ddeb4ab641ccec8e231c40ff1ae907
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-02-13 10:06:07 -0500

    update source catalog reference url

--- solutions/custom-deployable-arch/main.tf
+++ solutions/custom-deployable-arch/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
-  # source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&catalogID=7df1e4ca-d54c-4fd0-82ce-3d13247308cd&flavor=standard&kind=terraform&name=slz-vpc-with-vsis&version=1.10.3"
+  #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&catalogID=7df1e4ca-d54c-4fd0-82ce-3d13247308cd&flavor=standard&kind=terraform&name=slz-vpc-with-vsis&version=v2.0.0"
   source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v2.0.0"
   prefix           = var.prefix
   region           = "us-east"
