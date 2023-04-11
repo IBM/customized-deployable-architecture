085fa90551e945db146166cebfcd42fbfdcdc8a0
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-04-11 13:18:11 -0400

    use us-south for ys1

--- solutions/custom-deployable-arch/main.tf
+++ solutions/custom-deployable-arch/main.tf
@@ -6,7 +6,7 @@ module "landing_zone" {
   #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&catalogID=7df1e4ca-d54c-4fd0-82ce-3d13247308cd&flavor=standard&kind=terraform&name=slz-vpc-with-vsis&version=v2.0.0"
   source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v3.2.1"
   prefix           = var.prefix
-  region           = "us-east"
+  region           = "us-south"
   ibmcloud_api_key = var.ibmcloud_api_key
   ssh_public_key   = var.ssh_key
   override         = true
