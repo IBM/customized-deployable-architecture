05e56f232203ade2e5e5819c43a0fe7e8cb64f5d
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-04-27 13:32:10 -0400

    create new ys1 testing branch with updated main branch

--- solutions/custom-deployable-arch/babyslz/main.tf
+++ solutions/custom-deployable-arch/babyslz/main.tf
@@ -6,7 +6,7 @@ module "landing_zone" {
   #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v3.5.1"
   source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v3.2.1"
   prefix           = var.prefix
-  region           = "us-east"
+  region           = "us-south"
   ibmcloud_api_key = var.ibmcloud_api_key
   ssh_public_key   = var.ssh_key
   override         = true
