f01cbc849fd4d7cfdc64c77c2516e2f546c9f5c6
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-05-17 12:29:49 -0400

    refresh test branch for ys1

--- solutions/custom-slz/main.tf
+++ solutions/custom-slz/main.tf
@@ -5,7 +5,7 @@
 module "landing_zone" {
   #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v3.5.1"
   # for certain testing purposes use the following source.
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=3.5.1"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v3.5.1"
   prefix           = var.prefix
   region           = "us-south"
   ibmcloud_api_key = var.ibmcloud_api_key
