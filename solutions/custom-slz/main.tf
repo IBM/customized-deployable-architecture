42dea26cf0c353127a212e8917c455e642e169af
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-09-08 15:52:01 -0400

    update ys1 branch with newer slz version

--- solutions/custom-slz/main.tf
+++ solutions/custom-slz/main.tf
@@ -3,10 +3,11 @@
 ##############################################################################
 
 module "landing_zone" {
-  #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v3.5.1"
-  # for certain testing purposes use the following source.
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v3.5.1"
+  source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v4.4.7"
   prefix           = var.prefix
+  #
+  #  need us-south for ys1 - this is the only difference between release for prod env
+  #
   region           = "us-south"
   ibmcloud_api_key = var.ibmcloud_api_key
   ssh_public_key   = var.ssh_key
