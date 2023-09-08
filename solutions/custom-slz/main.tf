0bc0ce813fff9a9f5bed456e69bcb65de15aaf20
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-09-08 08:59:20 -0400

    slz 4.4.7

--- solutions/custom-slz/main.tf
+++ solutions/custom-slz/main.tf
@@ -3,9 +3,10 @@
 ##############################################################################
 
 module "landing_zone" {
-  #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v3.5.1"
+  #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source//patterns/vsi?archive=tgz&flavor=standard&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v4.4.7"
+  source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source?archive=tgz&flavor=standard&installType=fullstack&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v4.4.7"
   # for certain testing purposes use the following source.
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v3.5.1"
+  #source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v4.7.0"
   prefix           = var.prefix
   region           = "us-south"
   ibmcloud_api_key = var.ibmcloud_api_key
