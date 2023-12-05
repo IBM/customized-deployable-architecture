7cefa84208b8397b1163c130db87ee58b8ab6175
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-12-05 12:12:29 -0500

    create bootstrap script to ansible, add to outputs

---
+++ solutions/apache-workload/extension/playbook/generate-inventory.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+fip=$1
+webserver=$2
+private_key=$3
+
+echo "${private_key}" > keyfile
+chmod 600 keyfile
+
+echo "[jumpbox_public]"
+echo "my-public-bastion ansible_host=${fip}"
+echo " "
+
+echo "[webserver_private]"
+echo "my-webserver-target ansible_host=${webserver}"
+echo " "
+
+echo "[webserver_private:vars]"
+echo "ansible_ssh_common_args='-o ProxyCommand=\"ssh -W %h:%p -q root@${fip} -o IdentityFile=/Users/kbiegert/git/IBM/customized-deployable-architecture/solutions/apache-workload/extension/playbook/keyfile \"'"
+echo " "
+
+echo "[all:vars]"
+echo "ansible_ssh_private_key_file=/Users/kbiegert/git/IBM/customized-deployable-architecture/solutions/apache-workload/extension/playbook/keyfile " 
+echo " "
