04f8d5b91e7595d08690474967b045b77744c3d0
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-12-06 16:16:23 -0500

    add debugging for key

--- solutions/apache-workload/extension/playbook/generate-inventory.sh
+++ solutions/apache-workload/extension/playbook/generate-inventory.sh
@@ -1,11 +1,23 @@
 #! /bin/sh
-fip=$1
-webserver=$2
-private_key=$3
+fip="${1}"
+webserver="${2}"
+private_key="${3}"
 
+# step 0 - debug by dumping input values
+echo "-----begin generate script-------"
+echo "fip=${1}"
+echo "webserver=${2}"
+echo "private_key=${3}"
+echo "-----end debug output------------"
+
+# step 1 - create a file with the ssh private key value so that it may be used below with cli
+#
 echo "${private_key}" > keyfile
 chmod 600 keyfile
 
+# step 2 - create an ansible inventory file and fill in the ip addresses for the jump box and vsi
+#
+{
 echo "[jumpbox_public]"
 echo "my-public-bastion ansible_host=${fip}"
 echo " "
@@ -19,5 +31,6 @@ echo "ansible_ssh_common_args='-o ProxyCommand=\"ssh -W %h:%p -q root@${fip} -o 
 echo " "
 
 echo "[all:vars]"
-echo "ansible_ssh_private_key_file=./keyfile " 
+echo "ansible_ssh_private_key_file=./keyfile "
 echo " "
+} > inventory-test.txt
