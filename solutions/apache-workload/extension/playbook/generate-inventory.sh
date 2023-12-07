43105e6f7b1eb1df8084dddc918fedcf901ca145
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-12-07 11:39:16 -0500

    remove heredoc markers from the key

--- solutions/apache-workload/extension/playbook/generate-inventory.sh
+++ solutions/apache-workload/extension/playbook/generate-inventory.sh
@@ -15,8 +15,9 @@ echo "-----end debug output------------"
 #   - remove last line which is another heredoc marker
 #   - remove a leading newline character
 #   - key file permissions must be 600
-echo "${private_key}" | sed 's/<<-EOF//' | sed '$d' > keyfile.tmp
-tail -c +2 keyfile.tmp > keyfile
+echo "${private_key}" | sed 's/<<-EOF//' > keyfile1.tmp
+cat keyfile1.tmp | tail -r | tail -n +2 | tail -r > keyfile2.tmp
+tail -c +2 keyfile2.tmp > keyfile
 chmod 600 keyfile
 
 # step 2 - create an ansible inventory file and fill in the ip addresses for the jump box and vsi
