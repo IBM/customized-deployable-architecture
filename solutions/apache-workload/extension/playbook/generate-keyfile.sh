dd48c14f69b8c13959e051643913d98442a8a298
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2023-12-11 16:10:52 -0500

    clean up and documentation

--- solutions/apache-workload/extension/playbook/generate-keyfile.sh
+++ solutions/apache-workload/extension/playbook/generate-keyfile.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+# 
+# This script creates a file to contain an ssh private key value.  The file will be used as part of 
+# an ssh operation by ansible.
+#
+# The value passed to the script is expected to be in heredoc format (multi-line string).  The heredoc
+# notation needs to be removed and the file must end with a newline.
+#
+
 private_key="${1}"
 
 # The private key value was wrapped within heredoc notation to facilate passing a multi-line
