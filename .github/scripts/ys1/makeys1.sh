c690302f46803d246cc4fade21e4b6be58805fc7
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2024-01-22 10:34:57 -0500

    add ibm_catalog changes for ys1

--- .github/scripts/ys1/makeys1.sh
+++ .github/scripts/ys1/makeys1.sh
@@ -2,12 +2,12 @@
 
 echo "Making changes to BabySLZ variation enable successfull deployment on ys1"
 
-cd solutions/custom-deployable-arch/babyslz || exit
+cd solutions/custom-slz || exit
 
 echo "...modify main.tf"
 echo ".....change us-east to us-south"
 echo ".....change source reference url in main.tf"
-../../../.github/scripts/ys1/main-tf-changes.awk < main.tf > main.tf.new
+../../.github/scripts/ys1/main-tf-changes.awk < main.tf > main.tf.new
 cp main.tf.new main.tf
 rm main.tf.new
 
@@ -15,10 +15,11 @@ echo "...modify override.json"
 echo ".....change slz-service-rg to Default"
 echo ".....change slz-workload-rg to Default"
 echo ".....change resource group create settings in override.json"
-../../../.github/scripts/ys1/override-json-changes.awk < override.json > override.json.new
+../../.github/scripts/ys1/override-json-changes.awk < override.json > override.json.new
 cp override.json.new override.json
 rm override.json.new
 
+cd ../..
 echo "...modify ibm_catalog.json"
 echo ".....change \"name\": \"custom-deployable-arch\" to \"name\": \"deploy-arch-ibm-gm-test-slz\""
 sed -i '' 's/name\": \"custom-deployable-arch/name\": \"deploy-arch-ibm-gm-test-slz/g' ibm_catalog.json
