a197919f4faec30d2cfdd5e1a2edf33e0375f0d4
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2024-08-06 09:57:28 -0400

    allow TF runtime 1.6 and greater by using laterversions of GE. Use ibm-ubuntu-22-04-4-minimal-amd64-3

--- solutions/apache-workload/extension/variables.tf
+++ solutions/apache-workload/extension/variables.tf
@@ -3,7 +3,7 @@
 variable "image" {
   description = "Available images may be found by using ibmcloud is images cli command."
   type    = string
-  default = "ibm-ubuntu-22-04-4-minimal-amd64-1"
+  default = "ibm-ubuntu-22-04-4-minimal-amd64-3"
 }
 
 # API key with sufficient permissions to deploy resources 
