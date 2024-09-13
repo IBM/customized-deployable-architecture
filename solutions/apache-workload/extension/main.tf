263b27c952b861ea620a71706c5c313c43b85285
GABRIEL VALENCIA <gvalenc@us.ibm.com>
2024-09-13 13:47:54 -0500

    update lz vsi version

--- solutions/apache-workload/extension/main.tf
+++ solutions/apache-workload/extension/main.tf
@@ -4,19 +4,19 @@ locals {
 
   # Determine where the workspace is for the prerequisite deployment.  The location is the first part of the
   # pre-requisite workspace's id string.  The workspace id is not provided when running as a full stack so check for a null value.
-  location = var.prerequisite_workspace_id != "" ? regex("^[a-z/-]+", var.prerequisite_workspace_id ) : ""
+  location = var.prerequisite_workspace_id != "" ? regex("^[a-z/-]+", var.prerequisite_workspace_id) : ""
 }
 
 data "ibm_schematics_workspace" "schematics_workspace" {
   # only query the resource if a workspace id was given.  means this is running as an extension.
-  count  = var.prerequisite_workspace_id == "" ? 0 : 1
+  count        = var.prerequisite_workspace_id == "" ? 0 : 1
   workspace_id = var.prerequisite_workspace_id
   location     = local.location
 }
 
 data "ibm_schematics_output" "schematics_output" {
   # only query the resource if a workspace id was given.  means this is running as an extension.
-  count  = var.prerequisite_workspace_id == "" ? 0 : 1
+  count        = var.prerequisite_workspace_id == "" ? 0 : 1
   workspace_id = var.prerequisite_workspace_id
   location     = local.location
   template_id  = data.ibm_schematics_workspace.schematics_workspace[0].runtime_data[0].id
@@ -30,18 +30,18 @@ locals {
 }
 
 data "ibm_is_subnet" "subnet" {
-  count  = var.prerequisite_workspace_id == "" ? 0 : 1
-  name = local.subnet_name
+  count = var.prerequisite_workspace_id == "" ? 0 : 1
+  name  = local.subnet_name
 }
 
 data "ibm_is_ssh_key" "ssh-key" {
-  count  = var.prerequisite_workspace_id == "" ? 0 : 1
-  name = join("-", [local.prefix, "ssh-key"])
+  count = var.prerequisite_workspace_id == "" ? 0 : 1
+  name  = join("-", [local.prefix, "ssh-key"])
 }
 
 data "ibm_is_floating_ip" "jump-box-fip" {
-  count  = var.prerequisite_workspace_id == "" ? 0 : 1
-  name = join("-", [local.prefix, "jump-box-001-fip"])
+  count = var.prerequisite_workspace_id == "" ? 0 : 1
+  name  = join("-", [local.prefix, "jump-box-001-fip"])
 }
 
 # the image resource is always present and it is ok to query it.
@@ -50,10 +50,10 @@ data "ibm_is_image" "image" {
 }
 
 locals {
-  resource_group_id = var.resource_group_id != "" ? var.resource_group_id : data.ibm_is_subnet.subnet[0].resource_group
-  subnet_id  = var.subnet_id != "" ? var.subnet_id : data.ibm_is_subnet.subnet[0].id
-  vpc_id     = var.vpc_id != "" ? var.vpc_id : data.ibm_is_subnet.subnet[0].vpc
-  ssh_key_id = var.ssh_key_id != "" ? var.ssh_key_id : data.ibm_is_ssh_key.ssh-key[0].id
+  resource_group_id          = var.resource_group_id != "" ? var.resource_group_id : data.ibm_is_subnet.subnet[0].resource_group
+  subnet_id                  = var.subnet_id != "" ? var.subnet_id : data.ibm_is_subnet.subnet[0].id
+  vpc_id                     = var.vpc_id != "" ? var.vpc_id : data.ibm_is_subnet.subnet[0].vpc
+  ssh_key_id                 = var.ssh_key_id != "" ? var.ssh_key_id : data.ibm_is_ssh_key.ssh-key[0].id
   fp_vsi_floating_ip_address = var.fp_vsi_floating_ip_address != "" ? var.fp_vsi_floating_ip_address : data.ibm_is_floating_ip.jump-box-fip[0].address
 }
 
@@ -62,13 +62,13 @@ data "ibm_is_subnet" "by-subnet-id" {
 }
 
 module "slz_vsi" {
-  source                     = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vsi.git?ref=v3.2.1"
+  source                     = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vsi.git?ref=v3.3.0"
   resource_group_id          = local.resource_group_id
   image_id                   = data.ibm_is_image.image.id
   create_security_group      = true
   security_group             = var.appSecurityRules
   tags                       = ["apache"]
-  subnets                    = [{"name": local.subnet_name, "id": local.subnet_id, "zone":data.ibm_is_subnet.by-subnet-id.zone, "cidr": data.ibm_is_subnet.by-subnet-id.ipv4_cidr_block}]
+  subnets                    = [{ "name" : local.subnet_name, "id" : local.subnet_id, "zone" : data.ibm_is_subnet.by-subnet-id.zone, "cidr" : data.ibm_is_subnet.by-subnet-id.ipv4_cidr_block }]
   vpc_id                     = local.vpc_id
   prefix                     = join("-", [local.prefix, "apache-webserver"])
   machine_type               = "cx2-2x4"
